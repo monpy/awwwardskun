993fb24cee6934a0bc07206e24f5fb0880888d78
monpy <4696monpy@gmail.com>
2015-05-19 06:35:55 +0900

    不要な記述の削除

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,11 @@
 // 無名関数で呼ぶとどうなる？
 import "babelify/polyfill";
 import React from 'react';
-import Jsonp from 'jsonp';
 
 import Articles from './components/articles.jsx';
 import TumblrWebApiUtils from './utils/tumblr-webapi-utils.js';
 
-console.log(TumblrWebApiUtils);
-
 TumblrWebApiUtils.getArticleFromOffset();
-
 
 React.render(
     <Articles />,
